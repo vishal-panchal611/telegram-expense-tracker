1e1cc638a8a16a5d61e50d419e52cc177adba73a
vishal-panchal611 <vpanchal611@gmail.com>
2025-10-18 18:00:28 +0530

    Changed model of gemini from 1.5-flash to 2.0-flash

--- userService.js
+++ userService.js
@@ -4,7 +4,7 @@ import { supabase } from "./supabaseClient.js";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export async function registerUser(telegramId, username) {
   try {
