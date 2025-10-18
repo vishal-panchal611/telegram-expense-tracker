33c7603c654506c8bc9d78639117162c3b9e426b
vishal-panchal611 <vpanchal611@gmail.com>
2025-10-18 14:33:50 +0530

    Added changes for LLM model

--- userService.js
+++ userService.js
@@ -4,7 +4,7 @@ import { supabase } from "./supabaseClient.js";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-flash" });
 
 export async function registerUser(telegramId, username) {
   try {
